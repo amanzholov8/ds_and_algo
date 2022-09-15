7246e7e1df574967f020bccda7c9b1cbdc36dc85
Alim Amanzholov <alim.amanzholov@gmail.com>
2022-09-15 12:12:51 +0600

    change bst implementation, add tests

--- DS/BST.py
+++ DS/BST.py
@@ -1,49 +1,76 @@
-class Node:
+# binary search tree
+class BSTNode:
   def __init__(self, val):
     self.val = val
     self.left = None
     self.right = None
 
-class BST: # i.e. binary search tree
+  def insert(self, val):
+    if val == self.val:
+      raise Exception("Cannot insert duplicates into binary search tree")
+    elif val <= self.val:
+      if self.left:
+        self.left.insert(val)
+      else:
+        self.left = BSTNode(val)
+    else:
+      if self.right:
+        self.right.insert(val)
+      else:
+        self.right = BSTNode(val)
+
+  def contains(self, val):
+    if val == self.val:
+      return True
+    elif val < self.val:
+      if not self.left:
+        return False
+      return self.left.contains(val)
+    else:
+      if not self.right:
+        return False
+      return self.right.contains(val)
+
+  def printInOrder(self):
+    if self.left:
+      self.left.printInOrder()
+    print(self.val)
+    if self.right:
+      self.right.printInOrder()
+
+class BST:
   def __init__(self):
     self.root = None
 
   def insert(self, val):
     if not self.root:
-      self.root = Node(val)
-
-    if val <= self.root.val:
-      if self.root.left:
-        self.root.left.insert(val)
-      else:
-        self.root.left = Node(val)
-    else:
-      if self.root.right:
-        self.root.right.insert(val)
-      else:
-        self.root.right = Node(val)
+      self.root = BSTNode(val)
+      return
+    self.root.insert(val)
 
   def contains(self, val):
     if not self.root:
       return False
-
-    if val == self.root.val:
-      return True
-    elif val < self.roo.val:
-      if not self.root.left:
-        return False
-      return self.root.left.contains(val)
-    else:
-      if not self.root.right:
-        return False
-      return self.root.right.contains(val)
+    return self.root.contains(val)
 
   def printInOrder(self):
     if not self.root:
       print("None")
       return
-    if self.root.left:
-      self.root.left.printInOrder()
-    print(self.root.val)
-    if self.root.right:
-      self.root.right.printInOrder()
+    self.root.printInOrder()
+
+
+# tests
+bst = BST()
+bst.insert(10)
+assert(bst.contains(10))
+assert(bst.root.val == 10)
+bst.insert(12)
+assert(bst.contains(12))
+bst.insert(8)
+assert(bst.contains(8))
+bst.insert(11)
+assert(bst.contains(11))
+bst.insert(9)
+assert(bst.contains(9))
+bst.printInOrder()
